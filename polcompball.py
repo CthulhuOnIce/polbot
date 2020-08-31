9439a811f9423de77e25de46a922d018b32ac3db
CthulhuOnIce <cthulhuonice000@gmail.com>
2020-08-30 19:09:04 -0500

    try different mutations of ideology's name

--- polcompball.py
+++ polcompball.py
@@ -4,18 +4,19 @@ import sumy
 BLACKLISTEDSECTIONS = ["gallery", "animations"]
 
 def get_article_json(name):
-	name = name.title().replace(" ", "_")
-	djson = requests.get(f"https://polcompball.fandom.com/api/v1/Articles/Details?titles={name}").json()
-	if not len(djson["items"]):
-		return
-	identity = list(djson["items"].keys())[0] # get the id of the article
-	djson = djson["items"][identity]
-	j = requests.get(f"https://polcompball.fandom.com/api/v1/Articles/AsSimpleJson?id={identity}").json()
-	j["thumbnail"] = djson["thumbnail"]
-	j["title"] = djson["title"]
-	j["abstract"] = djson["abstract"]
-	j["url"] = f"https://polcompball.fandom.com/wiki/{name}"
-	return j
+	name = name.title()
+	for name in [name.replace(" ", "_"), name.replace(" ", "-"), name.replace(" ", "")]:
+		djson = requests.get(f"https://polcompball.fandom.com/api/v1/Articles/Details?titles={name}").json()
+		if not len(djson["items"]):
+			continue
+		identity = list(djson["items"].keys())[0] # get the id of the article
+		djson = djson["items"][identity]
+		j = requests.get(f"https://polcompball.fandom.com/api/v1/Articles/AsSimpleJson?id={identity}").json()
+		j["thumbnail"] = djson["thumbnail"]
+		j["title"] = djson["title"]
+		j["abstract"] = djson["abstract"]
+		j["url"] = f"https://polcompball.fandom.com/wiki/{name}"
+		return j
 
 def trim_article_json(articlejson):
 	if not articlejson:
